1207fe9b18689e20b6c9f417865877db2575bd8c
Uniliva Pereira <uniliva.pereira@brf-br.com>
2019-04-24 17:34:01 -0300

    Ajustando main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,21 +5,17 @@
       $scope.carregandoM = true;
       produtos_service.getAllProdutosByTipo().then(function (data) {
         $scope.carregandoM = false;
-        var prod = data.data;
-        
-              $scope.valores_produtos_grafico =  Object.values(prod);
-          
-              $scope.produtos_grafico = Object.keys(prod);
+        $scope.prods = data.data;
           
               var ctx_grafico = $("#grafico-estoque").get(0).getContext("2d");
               var chart_grafico = new Chart(ctx_grafico, {
                 type: 'bar',
                 data: {
-                    labels: $scope.produtos_grafico,
+                    labels: Object.keys( $scope.prods),
                     datasets: [
                     {
                         label: "Estoque de Alimentos",
-                        data: $scope.valores_produtos_grafico,
+                        data:  Object.values( $scope.prods),
                         borderColor: ['rgb(102, 0, 0)','rgb(76, 153, 0)', 'rgb(204, 102, 0)'],
                         backgroundColor: ['rgb(102, 0, 0)', 'rgb(76, 153, 0)', 'rgb(204, 102, 0)'],
                     }
